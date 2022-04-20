2e3c67b624b613a4612c47bc463b525e7ae3919f
Mark <markfaction@hotmail.com>
2022-04-21 02:29:40 +0530

    Delete obsolete files

--- graphql/core/FounderEventsLoadingStrategy.ts
+++
@@ -1,9 +0,0 @@
-import IEventsLoadingStrategy from "./Events/interfaces/IEventsLoadingStrategy";
-import { FeedType } from "common/enums";
-import { FeedItem } from "common/types";
-
-export default class FounderEventsLoadingStrategy implements IEventsLoadingStrategy {
-    public execute(feedType: typeof FeedType): Array<FeedItem> {
-        return [];
-    }
-}
